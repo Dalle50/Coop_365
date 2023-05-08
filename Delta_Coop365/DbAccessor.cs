5d8c1eec07ade0291b3d7b1c641ec4e43a61c43b
Daniel <daniel.htc.jacobsen@gmail.com>
2023-05-08 10:53:08 +0200

    wait

--- Delta_Coop365/DbAccessor.cs
+++ Delta_Coop365/DbAccessor.cs
@@ -6,8 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Microsoft.VisualStudio.Setup.Configuration;
 using System.IO;
+using System.Configuration;
+using Microsoft.VisualStudio.Setup.Configuration;
 
 namespace Delta_Coop365
 {
@@ -23,9 +24,9 @@ namespace Delta_Coop365
         /// "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\danie\\source\\repos\\Delta_Coop365\\Delta_Coop365\\Database1.mdf;Integrated Security=True"
         /// </summary>
         /// <param name="connString"></param>
-        public DbAccessor(string connString) 
+        public DbAccessor() 
         {
-            this.connString = connString;
+            this.connString = ConfigurationManager.ConnectionStrings["post"].ConnectionString;
             //this.connString =
             //    "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename="
             //    + GetSolutionPath()
