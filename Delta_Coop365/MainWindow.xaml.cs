484e5b2d17d2b414f17b090008a80bdaf323963f
Daniel <daniel.htc.jacobsen@gmail.com>
2023-05-08 13:08:31 +0200

    Working

--- Delta_Coop365/MainWindow.xaml.cs
+++ Delta_Coop365/MainWindow.xaml.cs
@@ -51,6 +51,15 @@ namespace Delta_Coop365
                     double price = (double)e.Element("Pris");
                     string name = (string)e.Element("Name");
                     string ingredients = (string)e.Element("Ingredience");
+
+                    object x = productid.GetType();
+                    object y = price.GetType();
+                    object u = name.GetType();
+                    object i = ingredients.GetType();
+                    Console.WriteLine(x);
+                    Console.WriteLine(y);
+                    Console.WriteLine(u);
+                    Console.WriteLine(i);
                     dbAccessor.insertIntoProducts(productid, name, ingredients, price);
                 }
             }
