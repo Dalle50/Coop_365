6c8ad21a94903a070f629509cd674a030ea5dd7a
Lynxexe <xgalaxykatx@gmail.com>
2023-05-12 11:08:33 +0200

    Various fixes

--- Delta_Coop365/Order.cs
+++ Delta_Coop365/Order.cs
@@ -79,5 +79,10 @@ namespace Delta_Coop365
             }
             TotalPrice = tempTotal;
         }
+        public List<OrderLine> GetOrderLines() { return orderLines; }
+        public void ClearOrderLines()
+        {
+            orderLines.Clear();
+        }
     }
 }
