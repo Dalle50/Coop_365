c09e5771b949a0ebf0e2562812fd38608c8081b7
Schnappsen <121798300+Schnappsen@users.noreply.github.com>
2023-05-31 21:00:39 +0200

    qwer

--- Delta_Coop365/QrCodeService.cs
+++ Delta_Coop365/QrCodeService.cs
@@ -10,7 +10,7 @@ namespace Delta_Coop365
     internal class QrCodeService
     {
         QRCodeGenerator qrGenerator;
-        public QrCodeService(int orderId)
+        public QrCodeService()
         {
             qrGenerator = new QRCodeGenerator();
         }
