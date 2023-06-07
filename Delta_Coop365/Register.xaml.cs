a062767872aa11d8120d614dcf6e10515aaa14a7
Daniel <daniel.htc.jacobsen@gmail.com>
2023-06-07 12:44:38 +0200

    xxx

--- Delta_Coop365/Register.xaml.cs
+++ Delta_Coop365/Register.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -12,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace Delta_Coop365
 {
